cfa0ca6a74e8c2e72642c49c25ad2ec8e98e3a73
Solokiller <Solokiller@gmail.com>
2023-12-10 17:30:36 +0200

    added build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# Check if the 'hello' executable exists and remove it if it does
 if [ -f min ]; then
     rm -f main
 fi
 
-# Compile the C program 'hello.c' and create an executable named 'hello'
 gcc main.c  -o main -lm
 
-# Run the compiled program 'hello'
 ./main
